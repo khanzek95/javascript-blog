b7dc10ba2f56767dbe390b1b62eb1e4367c70f3c
Kacper Hanżek <hanzyk1995@gmail.com>
2019-04-11 15:36:43 +0200

    Need help

--- js/script.js
+++ js/script.js
@@ -47,3 +47,44 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+
+  titleList.innerHTML = '';
+
+  /* for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+  for(let article of articles) {
+    article.classList.remove('active');
+  };
+
+    /* get the article id */
+
+    const articleId = this.getAttribute('id');
+     console.log(articleId);
+
+    /* find the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* get the title from the title element */
+
+    
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
