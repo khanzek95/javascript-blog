922b4e33fdfdb1b327d6f87bc27e72598ecdb7d3
Kacper Hanżek <hanzyk1995@gmail.com>
2019-04-17 13:54:37 +0200

    Add tags genetartor

--- js/script.js
+++ js/script.js
@@ -46,6 +46,7 @@ for(let activeLink of activeLinks){
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(){
 
@@ -94,3 +95,42 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+function generateTags(){
+  /* find all articles */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+ 
+  for(let article of articles) {
+  }
+    /* find tags wrapper */
+
+    const titleList = article.querySelector(optArticleTagsSelector);
+
+    /* make html variable with empty string */
+
+    let html = '';
+
+    /* get tags from data-tags attribute */
+
+    const articleTags = article.getAttribute('href');
+    console.log(articleTags);
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
